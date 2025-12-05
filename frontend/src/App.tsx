16f318842058e50e8c2498e9deff547ea6f3417b
Abhiraj028 <abhirajgautam028@gmail.com>
2025-12-05 09:00:00 +0530

    map bounds setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function App(){
+
+  
+
+  return(
+    <MapContainer
+      center={[31.9048, 77.1734]}
+      zoom={10}
+      scrollWheelZoom={true}
+      minZoom={9}
+      maxZoom={18}
+      maxBounds={[
+        [30.22,75.63],
+        [33.22,79.04]
+        ]}
+        maxBoundsViscosity={1}
+      className="map"
+    >
+      <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution="&copy; OpenStreetMap contributors"
+        detectRetina={true}
+        crossOrigin={true}
+      />
+
+
+    </MapContainer>
   )
 }
 
